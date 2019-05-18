76ef5ec5dbf3bd837486884f51d74a1e1da50dd9
seom <thseontw@gmail.com>
2019-05-18 21:35:25 +0900

    Create Customer Database Table & Node..js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -73,7 +73,8 @@ class App extends Component {
                     key={c.id}
                     id={c.id}
                     image={c.image}
-                    name={c.name}
+                    name={c.NAME}
+                    birthday={c.birthday}
                     gender={c.gender}
                     job={c.job} />
                 );
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,49 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port : conf.port,
+    database : conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '길라임',
-            'birthday': '011211',
-            'gender': '여',
-            'job': '직장인'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '강동원',
-            'birthday': '840212',
-            'gender': '남',
-            'job': '직장인'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '이하늬',
-            'birthday': '881213',
-            'gender': '여',
-            'job': '배우'
-        },
-        {
-            'id': 4,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '조보아',
-            'birthday': '920131',
-            'gender': '여',
-            'job': '유치원교사'
-        },
-        {
-            'id': 5,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '전현무',
-            'birthday': '750203',
-            'gender': '남',
-            'job': '간호장교'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
